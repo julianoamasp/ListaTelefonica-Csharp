89f053b9526b60ba0be825e06ddb11999a276719
Juliano da Silva Mendes <julianoamasp@gmail.com>
2021-07-15 22:55:06 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaTelefonica.Models
+{
+    public class Contato
+    {
+        public int ID { get; set; }
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string endereco { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ Models/ContatoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTelefonica.Models
+{
+    public class ContatoContext : DbContext
+
+    {
+        public ContatoContext(DbContextOptions<ContatoContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
